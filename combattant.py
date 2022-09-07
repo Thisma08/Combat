574e3af72a9b4ffc53f5e4ae7c69aaf40f2c1f4c
Thisma08 <77389133+Thisma08@users.noreply.github.com>
2022-09-07 14:44:52 +0200

    Images chargées apd'une spritesheet, les combattants se regardent

--- combattant.py
+++ combattant.py
@@ -1,12 +1,33 @@
 import pygame as pg
 
 class Combattant():
-    def __init__(self, x, y):
+    def __init__(self, x, y, flip, donnees, spritesheet, frames_animation):
         self.rect = pg.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.saut = False
         self.attaque = False
         self.type_attaque = 0
+        self.sante = 100
+        self.flip = flip
+        self.taille = donnees[0]
+        self.echelle_image = donnees[1]
+        self.decalage = donnees[2]
+        self.liste_animation = self.charger_images(spritesheet, frames_animation)
+        #0: debout / 1: course / 2: saut / 3: attaque1 / 4: attaque 2 / 5: coup pris / 6: ko
+        self.action = 0
+        self.indice_frame = 0
+        self.image = self.liste_animation[self.action][self.indice_frame]
+
+    def charger_images(self, spritesheet, frames_animation):
+        #Extraction d'images de la spritesheet
+        liste_animation = []
+        for y, animation in enumerate(frames_animation):
+            liste_im_temp = []
+            for x in range(animation):
+                im_temp = spritesheet.subsurface(x * self.taille, y * self.taille, self.taille, self.taille)
+                liste_im_temp.append(pg.transform.scale(im_temp, (self.taille * self.echelle_image, self.taille * self.echelle_image)))
+            liste_animation.append(liste_im_temp)
+        return liste_animation
 
     def bouger(self, larg_ecran, haut_ecran, surface, cible):
         VITESSE = 5
@@ -53,16 +74,24 @@ class Combattant():
             dy = haut_ecran - 110 - self.rect.bottom
             self.saut = False
 
+        #S'assurer que les 2 joueurs se font face
+        if cible.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
         #Maj de la position du joueur
         self.rect.x += dx
         self.rect.y += dy
 
     def dessiner(self, surface):
+        image = pg.transform.flip(self.image, self.flip, False)
         pg.draw.rect(surface, (0, 255, 0), self.rect)
+        surface.blit(image, (self.rect.x - (self.decalage[0] * self.echelle_image), self.rect.y - (self.decalage[1] * self.echelle_image)))
 
     def attaquer(self, surface, cible):
         self.attaque = True
-        rect_attaque = pg.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+        rect_attaque = pg.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
         if rect_attaque.colliderect(cible.rect):
-            print("Coup infligé")
+            cible.sante -= 10
         pg.draw.rect(surface, (255, 0, 0), rect_attaque)
--- main.py
+++ main.py
@@ -14,18 +14,49 @@ pg.display.set_caption("Combat")
 horloge = pg.time.Clock()
 fps = 60
 
+#Definir couleurs
+ROUGE = (255, 0, 0)
+JAUNE = (255, 255, 0)
+BLANC = (255, 255, 255)
+
+#Definir les variables de combattant
+GUERRIER_TAILLE = 162
+GUERRIER_ECHELLE = 4
+GUERRIER_DECALAGE = [72, 56]
+GUERRIER_DONNEES = [GUERRIER_TAILLE, GUERRIER_ECHELLE, GUERRIER_DECALAGE]
+MAGICIEN_TAILLE = 250
+MAGICIEN_ECHELLE = 3
+MAGICIEN_DECALAGE = [112, 107]
+MAGICIEN_DONNEES = [MAGICIEN_TAILLE, MAGICIEN_ECHELLE, MAGICIEN_DECALAGE]
+
+
+
 #Charger l'image de fond
 im_fond = pg.image.load("effets/images/fond/fond.jpg").convert_alpha()
+
+#Charger la spritesheet
+spritesheet_guerrier = pg.image.load("effets/images/guerrier/sprites/guerrier.png").convert_alpha()
+spritesheet_magicien = pg.image.load("effets/images/magicien/sprites/magicien.png").convert_alpha()
+
+#Definir le nombre d'images dans chaque animation
+GUERRIER_NBRE_FRAMES = [10, 8, 1, 7, 7, 3, 7]
+MAGICIEN_NBRE_FRAMES = [8, 8, 1, 8, 8, 3, 7]
 
 #Fonction dessinant le fond
 def dessiner_fond():
     im_fond_echelle = pg.transform.scale(im_fond, (LARG_ECRAN, HAUT_ECRAN))
     ecran.blit(im_fond_echelle, (0, 0))
 
+#Fonction dessinant les barres de vie
+def dessiner_barres(sante, x, y):
+    ratio = sante / 100
+    pg.draw.rect(ecran, BLANC, (x - 3, y - 3, 406, 36))
+    pg.draw.rect(ecran, ROUGE, (x, y, 400, 30))
+    pg.draw.rect(ecran, JAUNE, (x, y, 400 * ratio, 30))
 
 #Creer 2 instances de combattants
-combattant_1 = Combattant(200, 310)
-combattant_2 = Combattant(700, 310)
+combattant_1 = Combattant(200, 310, False, GUERRIER_DONNEES, spritesheet_guerrier, GUERRIER_NBRE_FRAMES)
+combattant_2 = Combattant(700, 310, True, MAGICIEN_DONNEES, spritesheet_magicien, MAGICIEN_NBRE_FRAMES)
 
 #Boucle de jeu
 lance = True
@@ -34,6 +65,10 @@ while lance:
 
     #Dessiner le fond
     dessiner_fond()
+
+    #Dessiner les barres de vie
+    dessiner_barres(combattant_1.sante, 20, 20)
+    dessiner_barres(combattant_2.sante, 580, 20)
 
     #Bouger les combattants
     combattant_1.bouger(LARG_ECRAN, HAUT_ECRAN, ecran, combattant_2)
