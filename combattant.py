682776415fc800a41f612aa0aa3a2e76ed8fe49e
Thisma08 <77389133+Thisma08@users.noreply.github.com>
2022-09-14 17:56:15 +0200

    Animations fonctionnelles + jouable à 2

--- combattant.py
+++ combattant.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 class Combattant():
-    def __init__(self, x, y, flip, donnees, spritesheet, frames_animation):
+    def __init__(self, joueur, x, y, flip, donnees, spritesheet, frames_animation):
         self.rect = pg.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.saut = False
@@ -17,6 +17,12 @@ class Combattant():
         self.action = 0
         self.indice_frame = 0
         self.image = self.liste_animation[self.action][self.indice_frame]
+        self.temps_de_maj = pg.time.get_ticks()
+        self.court = False
+        self.cooldown_attaque = 0
+        self.coup = False
+        self.vivant = True
+        self.joueur = joueur
 
     def charger_images(self, spritesheet, frames_animation):
         #Extraction d'images de la spritesheet
@@ -29,36 +35,66 @@ class Combattant():
             liste_animation.append(liste_im_temp)
         return liste_animation
 
-    def bouger(self, larg_ecran, haut_ecran, surface, cible):
+    def bouger(self, larg_ecran, haut_ecran, surface, cible, combat_termine):
         VITESSE = 5
         GRAVITE = 2
         dx = 0
         dy = 0
+        self.court = False
+        self.type_attaque = 0
 
         #Touches
         touche = pg.key.get_pressed()
 
         #Ne pas bouger quand attaque
-        if self.attaque == False:
-            #Mouvement
-            if touche[pg.K_q]:
-                dx = -VITESSE
-            if touche[pg.K_d]:
-                dx = VITESSE
+        #joueur 1
+        if self.joueur == 1:
+            if self.attaque == False and self.vivant == True and combat_termine == False:
+                #Mouvement
+                if touche[pg.K_q]:
+                    dx = -VITESSE
+                    self.court = True
+                if touche[pg.K_d]:
+                    dx = VITESSE
+                    self.court = True
 
-            #Saut
-            if touche[pg.K_z] and self.saut == False:
-                self.vel_y = -30
-                self.saut = True
+                #Saut
+                if touche[pg.K_z] and self.saut == False:
+                    self.vel_y = -30
+                    self.saut = True
 
-            #Attaque
-            if touche[pg.K_r] or touche[pg.K_t]:
-                self.attaquer(surface, cible)
-                #Type d'attaque
-                if touche[pg.K_r]:
-                    self.type_attaque = 1
-                if touche[pg.K_t]:
-                    self.type_attaque = 2
+                #Attaque
+                if touche[pg.K_r] or touche[pg.K_t]:
+                    self.attaquer(cible)
+                    #Type d'attaque
+                    if touche[pg.K_r]:
+                        self.type_attaque = 1
+                    if touche[pg.K_t]:
+                        self.type_attaque = 2
+
+        if self.joueur == 2:
+            if self.attaque == False and self.vivant == True and combat_termine == False:
+                #Mouvement
+                if touche[pg.K_LEFT]:
+                    dx = -VITESSE
+                    self.court = True
+                if touche[pg.K_RIGHT]:
+                    dx = VITESSE
+                    self.court = True
+
+                #Saut
+                if touche[pg.K_UP] and self.saut == False:
+                    self.vel_y = -30
+                    self.saut = True
+
+                #Attaque
+                if touche[pg.K_KP1] or touche[pg.K_KP2]:
+                    self.attaquer(cible)
+                    #Type d'attaque
+                    if touche[pg.K_KP1]:
+                        self.type_attaque = 1
+                    if touche[pg.K_KP2]:
+                        self.type_attaque = 2
 
         #Appliquer gravité
         self.vel_y += GRAVITE
@@ -80,18 +116,78 @@ class Combattant():
         else:
             self.flip = True
 
+        #Appliquer le cooldown d'attaque
+        if self.cooldown_attaque > 0:
+            self.cooldown_attaque -= 1
+
         #Maj de la position du joueur
         self.rect.x += dx
         self.rect.y += dy
 
     def dessiner(self, surface):
         image = pg.transform.flip(self.image, self.flip, False)
-        pg.draw.rect(surface, (0, 255, 0), self.rect)
         surface.blit(image, (self.rect.x - (self.decalage[0] * self.echelle_image), self.rect.y - (self.decalage[1] * self.echelle_image)))
 
-    def attaquer(self, surface, cible):
-        self.attaque = True
-        rect_attaque = pg.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
-        if rect_attaque.colliderect(cible.rect):
-            cible.sante -= 10
-        pg.draw.rect(surface, (255, 0, 0), rect_attaque)
+    def attaquer(self, cible):
+        if self.cooldown_attaque == 0:
+            self.attaque = True
+            rect_attaque = pg.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+            if rect_attaque.colliderect(cible.rect):
+                cible.sante -= 10
+                cible.coup = True
+
+    #Maj_animations
+    def maj(self):
+        #Quelle action le joueur fait-il ?
+        if self.sante <= 0:
+            self.sante = 0
+            self.vivant = False
+            self.maj_action(6)
+        elif self.coup == True:
+            self.maj_action(5)
+        elif self.attaque == True:
+            if self.type_attaque == 1:
+                self.maj_action(3)
+            elif self.type_attaque == 2:
+                self.maj_action(4)
+        elif self.saut == True:
+            self.maj_action(2)
+        elif self.court == True :
+            self.maj_action(1)
+        else:
+            self.maj_action(0)
+        #0: debout / 1: course / 2: saut / 3: attaque 1 / 4: attaque 2 / 5: coup pris / 6: ko
+
+        cooldown_anim = 50
+        self.image = self.liste_animation[self.action][self.indice_frame]
+
+        #Cheque si assez de temps est passé depuis la derniere maj
+        if pg.time.get_ticks() - self.temps_de_maj > cooldown_anim:
+            self.indice_frame += 1
+            self.temps_de_maj = pg.time.get_ticks()
+
+        #Cheque si l'animation est finie
+        if self.indice_frame >= len(self.liste_animation[self.action]):
+            #Si le joueur est mort alors ne plus l'animer
+            if self.vivant == False:
+                self.indice_frame = len(self.liste_animation[self.action]) - 1
+            else:
+                self.indice_frame = 0
+                #Cheque si une attaque est finie
+                if self.action == 3 or self.action == 4:
+                    self.attaque = False
+                    self.cooldown_attaque = 50
+                # Cheque si un coup a ete pris
+                if self.action == 5:
+                    self.coup = False
+                    #Si le joueur etait au milieu d'une attaque, celle ci est stoppée
+                    self.attaque = False
+                    self.cooldown_attaque = 20
+
+    def maj_action(self, nouv_action):
+        #La nouvelle action est elle differente de la precedente ?
+        if nouv_action != self.action:
+            self.action = nouv_action
+            #maj parametres d'animation
+            self.indice_frame = 0
+            self.temps_de_maj = pg.time.get_ticks()
--- main.py
+++ main.py
@@ -19,6 +19,22 @@ ROUGE = (255, 0, 0)
 JAUNE = (255, 255, 0)
 BLANC = (255, 255, 255)
 
+#Definition de la p
+police_c_a_r = pg.font.Font("effets/polices/turok.ttf", 200)
+police_score = pg.font.Font("effets/polices/turok.ttf", 30)
+
+#Fonction pour dessiner du texte
+def dessiner_texte(texte, police, couleur, x, y):
+    image = police.render(texte, True, couleur)
+    ecran.blit(image, (x, y))
+
+#Definition variables de jeu
+compte_rebours_intro = 3
+derniere_maj_compte_rebours = pg.time.get_ticks()
+score = [0, 0] #[j1, j2]
+combat_termine = False
+COOLDOWN_COMBAT_TERMINE = 3000
+
 #Definir les variables de combattant
 GUERRIER_TAILLE = 162
 GUERRIER_ECHELLE = 4
@@ -29,14 +45,15 @@ MAGICIEN_ECHELLE = 3
 MAGICIEN_DECALAGE = [112, 107]
 MAGICIEN_DONNEES = [MAGICIEN_TAILLE, MAGICIEN_ECHELLE, MAGICIEN_DECALAGE]
 
-
-
 #Charger l'image de fond
 im_fond = pg.image.load("effets/images/fond/fond.jpg").convert_alpha()
 
 #Charger la spritesheet
 spritesheet_guerrier = pg.image.load("effets/images/guerrier/sprites/guerrier.png").convert_alpha()
 spritesheet_magicien = pg.image.load("effets/images/magicien/sprites/magicien.png").convert_alpha()
+
+#Charger le texte de victoire
+im_vict = pg.image.load("effets/images/icones/victory.png").convert_alpha()
 
 #Definir le nombre d'images dans chaque animation
 GUERRIER_NBRE_FRAMES = [10, 8, 1, 7, 7, 3, 7]
@@ -55,8 +72,8 @@ def dessiner_barres(sante, x, y):
     pg.draw.rect(ecran, JAUNE, (x, y, 400 * ratio, 30))
 
 #Creer 2 instances de combattants
-combattant_1 = Combattant(200, 310, False, GUERRIER_DONNEES, spritesheet_guerrier, GUERRIER_NBRE_FRAMES)
-combattant_2 = Combattant(700, 310, True, MAGICIEN_DONNEES, spritesheet_magicien, MAGICIEN_NBRE_FRAMES)
+combattant_1 = Combattant(1, 200, 310, False, GUERRIER_DONNEES, spritesheet_guerrier, GUERRIER_NBRE_FRAMES)
+combattant_2 = Combattant(2, 700, 310, True, MAGICIEN_DONNEES, spritesheet_magicien, MAGICIEN_NBRE_FRAMES)
 
 #Boucle de jeu
 lance = True
@@ -69,13 +86,46 @@ while lance:
     #Dessiner les barres de vie
     dessiner_barres(combattant_1.sante, 20, 20)
     dessiner_barres(combattant_2.sante, 580, 20)
+    dessiner_texte("J1 : " + str(score[0]), police_score, ROUGE, 20, 60)
+    dessiner_texte("J2 : " + str(score[1]), police_score, ROUGE, 580, 60)
 
-    #Bouger les combattants
-    combattant_1.bouger(LARG_ECRAN, HAUT_ECRAN, ecran, combattant_2)
+
+    #Maj compte a rebours
+    if compte_rebours_intro <= 0:
+        #Bouger les combattants
+        combattant_1.bouger(LARG_ECRAN, HAUT_ECRAN, ecran, combattant_2, combat_termine)
+        combattant_2.bouger(LARG_ECRAN, HAUT_ECRAN, ecran, combattant_1, combat_termine)
+    else:
+        dessiner_texte(str(compte_rebours_intro), police_c_a_r, ROUGE, LARG_ECRAN / 2 - 40, HAUT_ECRAN / 3.5)
+        if (pg.time.get_ticks() - derniere_maj_compte_rebours) >= 700:
+            compte_rebours_intro -= 1
+            derniere_maj_compte_rebours = pg.time.get_ticks()
+
+    #Maj des combattants
+    combattant_1.maj()
+    combattant_2.maj()
 
     #Dessiner les combattants
     combattant_1.dessiner(ecran)
     combattant_2.dessiner(ecran)
+
+    #Cheque si un joueur est mort
+    if combat_termine == False:
+        if combattant_1.vivant == False:
+            score[1] += 1
+            combat_termine = True
+            fin_de_combat = pg.time.get_ticks()
+        elif combattant_2.vivant == False:
+            score[0] += 1
+            combat_termine = True
+            fin_de_combat = pg.time.get_ticks()
+    else:
+        ecran.blit(im_vict, (360, 150))
+        if pg.time.get_ticks() - fin_de_combat > COOLDOWN_COMBAT_TERMINE:
+            combat_termine = False
+            compte_rebours_intro = 3
+            combattant_1 = Combattant(1, 200, 310, False, GUERRIER_DONNEES, spritesheet_guerrier, GUERRIER_NBRE_FRAMES)
+            combattant_2 = Combattant(2, 700, 310, True, MAGICIEN_DONNEES, spritesheet_magicien, MAGICIEN_NBRE_FRAMES)
 
     #Gestion des evenements
     for evenement in pg.event.get():
